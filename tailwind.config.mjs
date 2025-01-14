58c4790d0275b1ffc1707185cefe533e170bdb5a
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2025-01-14 07:48:59 -0700

    [ADD] First configs of the project

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			'sans': ['"Red Hat Display"'],
+		},
+		colors: {
+			white: "#fff",
+			"cyan-600": "#4D96A9",
+			"cyan-300": "#8FE3F9",
+			"purple-600": "#855FB1",
+			"purple-300": "#D9B8FF",
+			"slate-900": "#28283D",
+			"slate-600": "#87879D",
+			"slate-300": "#D1D1DF",
+
+		},
+		extend: {},
+	},
+	plugins: [],
+}
